5131424960737dd625cb0e66fcbaea5a8cc930d5
Mollie Bootsma <mollie.bootsma@gmail.com>
2020-05-31 13:56:06 -0700

    add JS and HTML files

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+const add = function(number1, number2) {
+	return number1 + number2;
+};
+const number1 = parseInt(prompt("Enter a number"))
+alert(number1)
+const number2 = parseInt(prompt("Enter another number"))
+alert(add(number1, number2))
+
+/* alert(add(10,5)) */
